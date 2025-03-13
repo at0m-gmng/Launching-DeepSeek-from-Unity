a24b1725145fddadc799b4b9356472cd893ffcc2
DESKTOP-DAAB48R\ilya <maslov270396@mail.ru>
2025-03-13 12:37:52 +0700

    Add Process Service

---
+++ Assets/GameResources/Features/ProcessController/IJobObjectService.cs
@@ -0,0 +1,9 @@
+﻿namespace GameResources.Features.ProcessController
+{
+    using System.Diagnostics;
+
+    public interface IJobObjectService
+    {
+        bool AddProcess(Process process);
+    }
+}
---
+++ Assets/GameResources/Features/ProcessController/ProcessService.cs
@@ -0,0 +1,48 @@
+﻿namespace GameResources.Features.ProcessController
+{
+    using System;
+    using System.Diagnostics;
+    using Zenject;
+    using System.Threading.Tasks;
+    using Services.Scripts;
+
+    public class ProcessService : MonoInstaller, IService, IJobObjectService
+    {
+        protected IntPtr jobHandle;
+        
+        public override void InstallBindings() 
+            => Container.Bind<IService>().To<ProcessService>().FromComponentOn(gameObject).AsTransient();
+
+        public async Task<bool> TryRegister()
+        {
+            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+            {
+                jobHandle = WindowsJobObjectApi.CreateJob();
+                if (jobHandle != IntPtr.Zero)
+                {
+                    WindowsJobObjectApi.SetKillOnJobClose(jobHandle);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool AddProcess(Process process)
+        {
+            if (Environment.OSVersion.Platform != PlatformID.Win32NT || process == null || process.HasExited)
+                return false;
+
+            return WindowsJobObjectApi.AssignProcessToJob(jobHandle, process);
+        }
+        
+        protected virtual void Dispose()
+        {
+            if (jobHandle != IntPtr.Zero)
+            {
+                WindowsJobObjectApi.CloseJob(jobHandle);
+                jobHandle = IntPtr.Zero;
+            }
+        }
+    }
+}
